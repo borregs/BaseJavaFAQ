eaf069a9a4a89392564c7aaa9fbd2bf56191e7ee
Borrego Johnson <borregs@yopmail.com>
2018-06-07 06:18:54 +0000

    Update RegistroParalelo.java

--- RegistroParalelo.java
+++ RegistroParalelo.java
@@ -19,36 +19,43 @@ public class RegistroParalelo {
 	public static void main (String[] args) {
 
 		int NE = 1;
-		int[] nc={0}; 
-		String[] alumNames={""};
-		String[][] matNames={{""}};
-		int[] aluMaterias={0};
-		double[][] califs={};
+		int[] nc={0,0,0,0,0,0,0,0,0}; 
+		String[] alumNames={"","","","","","",""};
+		String[][] matNames={{"","","",""},{"","","",""},{"","","",""},{"","","","",""}};
+		int[] aluMaterias={0,0,0,0,0,0,0,0,0,0,0};
+		float[][] califs={{0,0,0},{0,0,0},{0,0,0},{0,0,0},{0,0,0},{0,0,0}};
 	
 		Scanner reader = new Scanner(System.in);
 		
-		System.out.print("Ingrese la cantidad de alumnos:");
+		System.out.println("Ingrese la cantidad de alumnos");
+		System.out.print(">>");
 		NE = reader.nextInt();
 		
 		for(int x=0;x<NE;x++){
 			
 			nc[x]=x;
 			
-			System.out.print("Ingrese el Nombre del alumno #" + x+1);
+			System.out.println("Ingrese el Nombre del alumno #" + (x+1));
+			System.out.print(">>");
 			alumNames[x] = reader.next();
 			
-			System.out.print("Ingrese la cantidad de materias que cursa "+ alumNames[x] +" Actualmente");
+			System.out.println("Ingrese la cantidad de materias que cursa "+ alumNames[x] +" Actualmente");
+			System.out.print(">>");
 			aluMaterias[x] = reader.nextInt();
 			
 			for(int y=0;y<aluMaterias[x];y++){
-				System.out.print("Ingrese el nombre de la materia #"+ y+1 +" de " + alumNames[x]);
+				System.out.println("Ingrese el nombre de la materia #"+ (y+1) +" de " + alumNames[x]);
+				System.out.print(">>");
 				matNames[x][y]=reader.next();
 				
 			
 				for(int j=0;j<3;j++){				
-					System.out.print("Ingrese la Calificacion #"+ j+1 +" de " + matNames[x][y]);
-					califs[x][j] = reader.nextInt();
+					System.out.println("Ingrese la Calificacion #"+ (j+1) +" de " + matNames[x][y]);
+					System.out.print(">>");
+					califs[x][y] += reader.nextFloat();
 					}
+					
+				califs[x][y]=califs[x][y]/3;
 			}
 			
 		}
@@ -57,6 +64,9 @@ public class RegistroParalelo {
 		System.out.println("----  ----------  ----------  -----");
 		
 				for(int x=0;x<NE;x++){
+
+						System.out.println((x+1) + " " + alumNames[x] + " " + aluMaterias[x] );
+
 
 				}
 		
