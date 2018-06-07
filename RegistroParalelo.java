b47ea41d68abde3de44936c8c8dd24d467b11a75
Borrego Johnson <borregs@yopmail.com>
2018-06-07 03:44:55 +0000

    Update RegistroParalelo.java

--- RegistroParalelo.java
+++ RegistroParalelo.java
@@ -9,12 +9,36 @@
  * 
  */
 
+import java.util.Scanner; 
+//libreria para leer desde STDIN (teclado)
 
 public class RegistroParalelo {
+
+
+	public int NE=1;
+	private int	nc[NE]={0}; 
+	private String alumNames[NE]={" "};
+	private int aluMaterias[NE]={" "};
+	private float califs[NE][3]={0.0,0.0};
 	
 	public static void main (String[] args) {
+		Scanner reader = new scanner(System.in);
 		
+		System.out.print("Ingrese la cantidad de alumnos:");
+		NE = reader.nextInt();
 		
+		for(int x=0;x<NE;x++){
+			
+			nc[x]=x;
+			
+			System.out.print("Ingrese el Nombre del alumno #" + x+1);
+			String alumNames[x] = reader.next();
+			
+			System.out.print("Ingrese la cantidad de materias que cursa "+ alumNames[x] +" Actualmente";
+			String aluMaterias[x] = reader.nextInt();
+			
+			
+		}
 		
 		
 		
