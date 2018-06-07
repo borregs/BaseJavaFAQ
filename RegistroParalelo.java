2c49ba452cb409eaf579e2c0c79316f0134a4437
Borrego Johnson <borregs@yopmail.com>
2018-06-07 03:03:32 +0000

    Create RegistroParalelo.java

---
+++ RegistroParalelo.java
@@ -0,0 +1,24 @@
+/*
+ * RegistroParalelo.java
+ * 
+ * En un arreglo guardar el numero de control de N estudiantes
+ * En otro guardar los Nombres
+ * En un arreglo uni dimendional las materias que llevan (preguntar)
+ * En otro bi-dimensional, guradar las 3 calif de cada clase de las N materias
+ * Imprimir todo incluyendo y mencionar al estudiante del mejor promedio 
+ * 
+ */
+
+
+public class RegistroParalelo {
+	
+	public static void main (String[] args) {
+		
+		
+		
+		
+		
+		
+	}
+}
+
